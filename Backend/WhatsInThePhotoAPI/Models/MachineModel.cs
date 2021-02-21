d71dfb588fe341d75d1a66fa005d14e9d5c84abb
OJARGR200 <OJARGR200@caledonian.ac.uk>
2021-02-21 14:09:37 +0000

    Created new controller and cleaned up in the files

---
+++ Backend/WhatsInThePhotoAPI/Models/MachineModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WhatsInThePhotoAPI.Models
+{
+    public class MachineModel
+    {
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string ModelLocation { get; set; }
+    }
+}
--- Backend/WhatsInThePhotoAPI/Startup.cs
+++ Backend/WhatsInThePhotoAPI/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WhatsInThePhotoAPI
 {
@@ -26,11 +19,10 @@ namespace WhatsInThePhotoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WhatsInThePhotoAPI", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "WhatsInThePhotoAPI", Version = "v1"});
             });
         }
 
@@ -50,10 +42,7 @@ namespace WhatsInThePhotoAPI
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
