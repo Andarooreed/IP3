bfdc03c3068b2bfc0926ea62b548d6c8f322a7ab
Andy Reid <andyreid1993@gmail.com>
2021-02-27 19:21:39 +0000

    tweaked success notification styling

--- Web/notifications/auth.notifications.php
+++ Web/notifications/auth.notifications.php
@@ -1,6 +1,6 @@
 <div class="signupalert success hide">
     <span class="fas fa-user-circle"></span>
-    <span class="msg">Account created</span>
+    <span class="msg"><strong>Account created</strong></span>
     <span class="close-btn">
         <span class="fas fa-times"></span>
     </span>
@@ -8,7 +8,7 @@
 
 <div class="loginalert success hide">
     <span class="fas fa-lock-open"></span>
-    <span class="msg">Login successful</span>
+    <span class="msg"><strong>Login successful</strong></span>
     <span class="close-btn">
         <span class="fas fa-times"></span>
     </span>
@@ -16,7 +16,7 @@
 
 <div class="logoutalert success hide">
     <span class="fas fa-lock"></span>
-    <span class="msg">Logout successful</span>
+    <span class="msg"><strong>Logout successful</strong></span>
     <span class="close-btn">
         <span class="fas fa-times"></span>
     </span>
