2bf726bcdb6029784251390656c905d5d5ceb547
RuaridhBurns <burnsruaridh@gmail.com>
2021-03-09 20:32:01 +0000

    info page stuff

---
+++ Web/modals/info.modal.php
@@ -0,0 +1,16 @@
+<!-- The Modal -->
+<div id="infomodal" class="modal">
+  <span onclick="document.getElementById('infomodal').style.display='none'" class="close" title="Close Modal">&times;</span>
+
+<!-- Modal Content -->
+<h3>The Team</h3>
+<ul>
+<li>Andrew Reid</li> 
+<li>Ruaridh Burns</li> 
+<li>Emily knight-rowley</li> 
+<li>Greame Whyte</li> 
+<li>Oscar Jargren Daniel McKain </li>
+</ul>
+
+<img src="./images/GCU.png" alt="Glasgow Caledonian University logo">
+</div>
