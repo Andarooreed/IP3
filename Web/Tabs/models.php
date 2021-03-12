a74ee68cea2cfffab4538daf154315bde7d94ca1
Andy Reid <andyreid1993@gmail.com>
2021-03-12 17:09:11 +0000

    push

--- Web/Tabs/models.php
+++ Web/Tabs/models.php
@@ -6,7 +6,7 @@ $arrContextOptions = array(
 	),
 );
 
-$response = file_get_contents("https://localhost:5001/api/MachineModel", false, stream_context_create($arrContextOptions));
+$response = file_get_contents("https://localhost:44317/api/MachineModel", false, stream_context_create($arrContextOptions));
 
 $json_array=json_decode($response,true); 
  function display_array_recursive($json_rec){
@@ -16,7 +16,8 @@ $json_array=json_decode($response,true);
 					display_array_recursive($value);
 					echo '<br>';
 				}else{
-					echo $key.': '.$value.'<br>';
+					$output =$key.': '.$value.'<br>';
+					echo $output;
 				}	
 			}	
 		}
@@ -24,4 +25,4 @@ $json_array=json_decode($response,true);
 	?>
 	<div class=model-info-test-display>
 	<?php display_array_recursive($json_array); ?>
-	</div>	
+	</div>
