93b54948ea0a5344e3d877c2c8246fdcc8af70b4
Andy Reid <andyreid1993@gmail.com>
2021-03-13 11:37:12 +0000

    System can now display single array elements from JSON

--- Web/Tabs/models.php
+++ Web/Tabs/models.php
@@ -8,21 +8,36 @@ $arrContextOptions = array(
 
 $response = file_get_contents("https://localhost:44317/api/MachineModel", false, stream_context_create($arrContextOptions));
 
-$json_array=json_decode($response,true); 
- function display_array_recursive($json_rec){
-		if($json_rec){
-			foreach($json_rec as $key=> $value){
-				if(is_array($value)){
-					display_array_recursive($value);
-					echo '<br>';
-				}else{
-					$output =$key.': '.$value.'<br>';
-					echo $output;
-				}	
-			}	
+$json_array = json_decode($response, true);
+
+
+function display_names($json_rec)
+{
+	if ($json_rec) {
+		foreach ($json_rec as $value) {
+			if (is_array($value)) {
+				$output = $value['name'] . '<br>';
+				echo $output;
+			}
 		}
 	}
-	?>
-	<div class=model-info-test-display>
-	<?php display_array_recursive($json_array); ?>
-	</div>
+}
+
+// function display_array_recursive($json_rec){
+// 	if($json_rec){
+// 		foreach($json_rec as $key=> $value){
+// 			if(is_array($value)){
+// 				display_array_recursive($value);
+// 				echo '<br>';
+// 			}else{
+// 				$output =$key.': '.$value.'<br>';
+// 				echo $output;
+// 			}	
+// 		}	
+// 	}
+// }
+
+?>
+<div class=model-info-test-display>
+	<?php display_names($json_array) ?>
+</div>
