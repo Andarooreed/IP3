75f055830a351b41507d280ac2644f6ceabf9b13
Andy Reid <andyreid1993@gmail.com>
2021-03-22 19:45:48 +0000

    Changed structure so that index content is written on index.php instead of header.php for continuity reasons

--- Web/header.php
+++ Web/header.php
@@ -52,26 +52,6 @@ session_start();
                         </div>
                         <li><a href="includes/logout.inc.php"><button class="button-one">Logout</button></a></li>
                     </div>
-                    <div class="leftside-nav" id="leftside-nav">
-                        <a href="javascript:void(0)" class="closebtn" id="closebtn" style="display: none;" onclick="closeNav()">&times;</a>
-                        <div class="title">
-                            <h3><strong><i class="fas fa-box-open" id="open-nav" onclick="openNav()"></i> &nbsp;&nbsp;&nbsp; What's in the box?</strong></h3>
-                        </div>
-                        <div class="left-nav-items">
-                            <a href="#"><i class="fas fa-columns"></i><span class="leftside-nav-wording" style="display: none;">&nbsp;&nbsp;&nbsp;Dashboard</span></a>
-                            <a href="#"><i class="fas fa-images"></i><span class="leftside-nav-wording" style="display: none;">&nbsp;&nbsp;&nbsp;Models</span></a>
-                            <a href="#"><i class="fas fa-cogs"></i><span class="leftside-nav-wording" style="display: none;">&nbsp;&nbsp;&nbsp;Settings</span></a>
-                        </div>
-                        <div class="leftside-nav-footer">
-                        <a href="#"><i class="fas fa-info-circle"></i></i><span class="leftside-nav-wording" style="display: none;">&nbsp;&nbsp;&nbsp;About</span></a>
-                        </div>
-                    </div>
-                    <div id="main">
-                    <?php
-                        include_once './Tabs/models.php';
-                    ?>
-                    </div>
-
                 <?php } else {
                 ?>
                     <div class="nav-right">
