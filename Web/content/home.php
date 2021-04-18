8f738b764cdaf344ada7170b1bec2bbbca54d492
DanielMcKain <danielmckain@gmx.com>
2021-04-18 23:28:15 +0100

    Image bug fix and home update

--- Web/content/home.php
+++ Web/content/home.php
@@ -1,20 +1,50 @@
 <div class="home-display">
 
 	<h1 style="text-align: center;">What's in the photo?</h1>
-	<br>
+
+	<p style="text-align: center;"><img id="user_model_img_3" src="images\Splash_Flash.png" style="height:400px;" alt="E Knight-Rowley S1802083" title="E Knight-Rowley S1802083"> </p>
+
 	<p style="text-align: justify;">“What’s in The photo?” uses a hands-on, practical approach that goes the extra step beyond theory work.
-		Instead of poring over textbooks or skimming articles for hours, you can learn how Machine Learning (ML) works by training and testing your very own image-identifying ML models!<br><br>
-		<strong><U>So what does this mean?</U></strong><br>
-		What's in the photo? is designed to allow you to train your own image classification models and run them right here on the website! Simply navigate to the models page and click the 'Add model' button to get started. Give it a name and upload your data set (The more images the better, go nuts, give it thousands to increase the accuracy) and away you go!<br>
-		Your data set should focus on one object, for example, if I upload 1000 images of a teacup the model will train itself to understand what a teacup looks like. If you're short on images don't worry, simply select to pull in images from the web and it will scrape Google for you!<br><br>
-		Training a model can take a significant amount of time (hours depending on how many images you've given it to work with). So we have added some pre-trained models to get you started.<br><br>
-		<strong><u>TensorFlow</u></strong><br>
-		If you've ever looked into ML in the past, the chances are that you've come across the term TensorFlow. So what is it?
-		TensorFlow is an open source framework for ML that uses the Python programming language. It makes getting into ML accessible to everyone and easier than ever!<br>
-		Since TensorFlow is open source, it has a wide array of libraries available to help you achieve your goals. What's in the photo? Makes use of this by developing it's core system on the Keras library
+		Instead of poring over textbooks or skimming articles for hours, you can learn how Machine Learning (ML) works by training and testing your very own image-identifying ML models!
 		<br><br>
-		<a href="https://www.tensorflow.org/">To read more on TensorFlow you can find information on their official website</a><br><br>
-		<strong><U>Keras</U></strong><br>
+		<strong><U>So what does this mean?</U></strong>
+		<br>
+		What’s in the photo? Is designed to allow you to train your own image classification models and run them right here on the website!
+		<br>
+		<ul>
+			<li>
+			See how a completed image-identification model works with our range of pre-trained models, available right within your model library
+			</li>
+			<li>
+			Create your own models and train them to recognise objects - you can use your own images to do this, get some help from our image downloading bot, or use a combination of both!
+			</li>
+			<li>
+			Test and run the models you create and watch them analyse new images to identify the object you’ve trained them to spot
+			</li>
+		</ul>
+		
+		<strong><u>TensorFlow</u></strong>
+		<br>
+		If you've ever looked into ML in the past, the chances are that you've come across the term TensorFlow. 
+		<br>
+		So what is it?
+		<br>
+		TensorFlow is an open source framework that makes getting into ML accessible to everyone and easier than ever!
+		<br>
+		Since TensorFlow is open source, it has a wide array of libraries available to help you achieve your goals. What's in the photo? Makes use of this by developing it's core system on the Keras library.
+		<br><br>
+		To read more on TensorFlow you can find information on their <a href="https://www.tensorflow.org/">official website</a>.
+		<br><br>
+		<strong><U>Keras</U></strong>
+		<br>
+		Machine Learning is <i> complicated </i> and makes use of very complex maths. 
+		
+		TensorFlow removes a lot of that complication by providing a framework to develop from - lowering the starting knowledge required to build a good solution.
+		...It's still pretty alien to people that aren't heavily in the field though.
+		
+		Keras is a framework built on top of TensorFlow which adds another layer of abstraction - once again lowering this barrier to entry and providing a (relatively!) simple foundation to start learning and developing Machine Learning solutions.
+		
+		TensorFlow is written at core in the hyper efficient "C" programming language and Keras provides a Python wrapper which is used behind the scenes in What's In The Box - as the brains behind the whole show!
 	</p>
 </div>
 </div>
--- Web/content/models.php
+++ Web/content/models.php
@@ -5,8 +5,9 @@ $arrContextOptions = array(
         "verify_peer_name" => false,
     ),
 );
-
-$response = file_get_contents("https://localhost:44317/api/MachineModel", false, stream_context_create($arrContextOptions));
+$user_id = $_SESSION["user_id"];
+$response = file_get_contents("https://localhost:44317/api/MachineModel?userId=$user_id", false, stream_context_create($arrContextOptions));
+//$response = file_get_contents("https://localhost:44317/api/MachineModel", false, stream_context_create($arrContextOptions));
 
 $json_array = json_decode($response, true);
 
@@ -14,12 +15,12 @@ $json_array = json_decode($response, true);
 <div class="leftside-modelnav">
     <h3 id="model-nav-title"><strong>Models</strong></h3>
     <?php
-    // When building the list of all available models, gather the first 3 image locations of each, encode the resultant array and pop it in a cookie
-    // Can then reference that cookie in Js to pull out the required locations... Hopefully
+    // When building the list of all available models, gather the first 3 image locations of each, encode the resultant array and pop it in a hidden div
+    // Can then reference that div in Js to pull out the required locations... Hopefully
     $key_path = [];
     $root_path = "..\\Backend\\WhatsInThePhotoAPI\\UserUploads\\";
     foreach ($json_array as $model) {
-        $source_folder = $model['modelId'] . "-" . $_SESSION["username"] . "-" . $model['name'];
+        $source_folder = strval($model['modelId']) . "-" . $_SESSION["username"] . "-" . explode(".",explode("-",$model['name'])[1])[0];
 
         if (isset($source_folder)) {
 
@@ -29,18 +30,19 @@ $json_array = json_decode($response, true);
             $cnt = 0;
             foreach ($files as $curr_file) {
                 if ($cnt++ > 2) {
-                    break;
-                }
+                    break;                }
                 // array_push($key_path, [$model['modelId'], $curr_file]);
+               // array_push($image_paths, $curr_file);
                 array_push($image_paths, $curr_file);
             }
             $key_path[$model['modelId']] = $image_paths;
+            //$key_path[$model['modelId']] = $source_folder;
         }
 
     ?>
         <div class="display-models">
-            <input style="display: none;" type="radio" id="<?php echo $model['modelId'] ?>" name="select" value="<?php echo $model['name'] ?>">
-            <label for="<?php echo $model['modelId'] ?>" class="model-selector"><?php echo $model['name'] ?></label>
+            <input style="display: none;" type="radio" id="<?php echo $model['modelId'] ?>" name="select" value="<?php echo $model['name']; ?>">
+            <label for="<?php echo $model['modelId'] ?>" class="model-selector"><?php echo $model['name']; ?></label>
         </div>
     <?php
     }
@@ -58,7 +60,7 @@ $json_array = json_decode($response, true);
 <div class="middle-box" style="display:none;">
     <i class="fas fa-images fa-5x"></i>
     <h3 id="model-title">Select a model from the side menu</h3>
-    <p id="model_image_array" hidden> <?php echo $model_cookie; ?> </p>
+    <p id="model_image_array" > <?php echo $model_cookie; ?> </p>
     <div class="model-contents-placeholder">
         <div class="content-image-placeholder">
             <img id="user_model_img_1" src="" style="height:124px; width:150px; border-radius:4px">
